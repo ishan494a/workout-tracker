d7bf2bce0e65f12765a8b1b549a719947c4f74f1
ishanuwo <ishanuwo@github.com>
2025-01-20 18:47:27 -0500

    changed config

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const ENV = "dev";
 const sequelize = new Sequelize({
-  host: ENV === "prod" ? process.env.DB_HOST : process.env.DB_HOST_LOCAL,
-  port: ENV === "prod" ? process.env.DB_PORT : process.env.DB_PORT_LOCAL,
-  database: ENV === "prod" ? process.env.DB_NAME : process.env.DB_NAME_LOCAL,
-  username: ENV === "prod" ? process.env.DB_USER : process.env.DB_USER_LOCAL,
-  password: ENV === "prod" ? process.env.DB_PASSWORD : process.env.DB_PASSWORD_LOCAL,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   dialect: 'postgres',
   logging: false,
 });
